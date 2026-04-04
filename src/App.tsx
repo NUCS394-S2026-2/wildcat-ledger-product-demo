aad9935f8ccf61df1de7b9dac94a797eec6a72e0
dudu-theman <dustanley07@gmail.com>
2026-04-04 14:44:16 -0500

    remove archived components

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,23 @@
 import './App.css';
 
-import { useRef, useState } from 'react';
+import { useState } from 'react';
 
-import { ActivityFeed } from './components/ActivityFeed';
 import { BudgetLineSummary } from './components/BudgetLineSummary';
 import { FilterBar } from './components/FilterBar';
 import { Header } from './components/Header';
-import { SummaryProgressBars } from './components/SummaryProgressBars';
 import { TransactionList } from './components/TransactionList';
 import { TransactionModal } from './components/TransactionModal';
 import { LedgerProvider } from './context/LedgerContext';
 
 const Dashboard = () => {
   const [modalOpen, setModalOpen] = useState(false);
-  const tableRef = useRef<HTMLDivElement>(null);
-
-  const scrollToTable = () => {
-    tableRef.current?.scrollIntoView({ behavior: 'smooth' });
-  };
 
   return (
     <div className="wl-app">
       <Header onAddTransaction={() => setModalOpen(true)} />
       <main className="wl-main">
-        <SummaryProgressBars />
         <BudgetLineSummary />
-        <ActivityFeed onViewAll={scrollToTable} />
-        <div ref={tableRef}>
-          <FilterBar />
-        </div>
+        <FilterBar />
         <TransactionList />
       </main>
       <TransactionModal isOpen={modalOpen} onClose={() => setModalOpen(false)} />
